ca202765489be0803ff61ecd459e87d9ea4f54f8
Awss Bwaneh <awsbwaneh3@gmail.com>
2025-07-04 22:31:11 +0300

    Final update

--- rush03.c
+++ rush03.c
@@ -2,21 +2,18 @@
 void ft_putchar(char c);
 
 void rush(int x, int y){
-    for(int i= 1; i<=y; i++){
-        if(i==1 || i==y){
-            for(int j= 1; j<=x; j++){
-                if(j==1) ft_putchar('A');
-                else if(j==x) ft_putchar('C');
-                else ft_putchar("B");
+    for (int j = 1; j <= y; j++) {
+        for (int i = 1; i <= x; i++) {
+            if (j==1 || j==y) {
+                if (i==1) ft_putchar('A');
+                else if (i == x) ft_putchar('C');
+                else ft_putchar('B');
             }
-            ft_putchar('\n');
-        }
-        else {
-            for(int j= 1; j<=x; j++){
-                if(j==1 || j==x) ft_putchar('B');
+            else {
+                if (i==1 || i==x) ft_putchar('B');
                 else ft_putchar(' ');
             }
-            ft_putchar('\n');
         }
+        ft_putchar('\n');
     }
 }
