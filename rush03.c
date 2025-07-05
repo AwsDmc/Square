7b2f74025112fc8d5ba670bf3adec4a51b5d1bda
Awos Ba'oni <awos.baoni@c1r2s3.irbid.42.school>
2025-07-05 11:55:00 +0000

    Final update

--- rush03.c
+++ rush03.c
@@ -1,19 +1,42 @@
-#include <unistd.h>
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awos.baoni <awos.baoni@learner.42.tech>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 07:19:46 by awos.baoni        #+#    #+#             */
+/*   Updated: 2025/07/05 11:43:12 by awos.baoni       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void rush(int x, int y){
-    for (int j = 1; j <= y; j++) {
-        for (int i = 1; i <= x; i++) {
-            if (j==1 || j==y) {
-                if (i==1) ft_putchar('A');
-                else if (i == x) ft_putchar('C');
-                else ft_putchar('B');
-            }
-            else {
-                if (i==1 || i==x) ft_putchar('B');
-                else ft_putchar(' ');
-            }
-        }
-        ft_putchar('\n');
-    }
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	row;
+	int	col;
+
+	row = 1;
+	while (row <= y)
+	{
+		col = 1;
+		while (col <= x)
+		{
+			if ((row == 1 || row == y) && (col == 1))
+				ft_putchar('A');
+			else if ((row == 1 || row == y) && (col == x))
+				ft_putchar('C');
+			else if (((row != 1 && row != y) && (col == 1 || col == x))
+				|| ((row == 1 || row == y) && (col != 1 && col != x)))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			col++;
+		}
+		ft_putchar('\n');
+		row++;
+	}
 }
